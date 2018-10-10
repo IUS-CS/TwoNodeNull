6f803c11eab74564bfdc9db279c254940adf483c
AMeiners <Andrew.meiners@gmail.com>
2018-10-10 17:18:01 -0400

    Adding BrewMVC. Needs work on setting up view to Brew Instead of Home controller

---
+++ BrewskiiTesting/BrewskiiApp/BrewskiiApp/Controllers/BrewController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BrewskiiApp.Controllers
+{
+    public class BrewController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ BrewskiiTesting/BrewskiiApp/BrewskiiApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrewskiiApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
